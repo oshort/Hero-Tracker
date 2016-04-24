fd48ed16d1637e176639f996932e3e69b1f928a5
Oliver Short <oshort55@gmail.com>
2016-04-24 15:43:06 -0400

    Initial Commit

---
+++ HeroTracker/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HeroTracker
+//
+//  Created by Oliver Short on 4/24/16.
+//  Copyright © 2016 Oliver Short. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
